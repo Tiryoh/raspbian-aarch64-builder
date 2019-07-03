e0d57abb332eefd36b8978a61628639b8880f6a3
Tiryoh <tiryoh@gmail.com>
2019-07-03 20:25:31 +0900

    busterでは32bitも64bitもSSDから起動できなかったのでstretchでイメージ作成

--- build.sh
+++ build.sh
@@ -12,9 +12,13 @@ cd $SRC_DIR
 cd $SRC_DIR/linux
 KERNEL=kernel7
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- O=$SRC_DIR/kernel bcmrpi3_defconfig
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- O=$SRC_DIR/kernel Image modules dtbs -j4
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- O=$SRC_DIR/kernel Image modules dtbs -j8
 cd $SRC_DIR/Downloads
-wget -c http://ftp.jaist.ac.jp/pub/raspberrypi/raspbian/images/raspbian-2019-06-24/2019-06-20-raspbian-buster.zip.sha256
-wget -c http://ftp.jaist.ac.jp/pub/raspberrypi/raspbian/images/raspbian-2019-06-24/2019-06-20-raspbian-buster.zip
-sha256sum -c 2019-06-20-raspbian-buster.zip.sha256
-unzip 2019-06-20-raspbian-buster.zip
+# wget -c http://ftp.jaist.ac.jp/pub/raspberrypi/raspbian/images/raspbian-2019-06-24/2019-06-20-raspbian-buster.zip.sha256
+# wget -c http://ftp.jaist.ac.jp/pub/raspberrypi/raspbian/images/raspbian-2019-06-24/2019-06-20-raspbian-buster.zip
+# sha256sum -c 2019-06-20-raspbian-buster.zip.sha256
+# unzip 2019-06-20-raspbian-buster.zip
+wget -c http://ftp.jaist.ac.jp/pub/raspberrypi/raspbian/images/raspbian-2019-04-09/2019-04-08-raspbian-stretch.zip.sha256
+wget -c http://ftp.jaist.ac.jp/pub/raspberrypi/raspbian/images/raspbian-2019-04-09/2019-04-08-raspbian-stretch.zip
+sha256sum -c 2019-04-08-raspbian-stretch.zip.sha256
+unzip 2019-04-08-raspbian-stretch.zip
